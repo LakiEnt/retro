6d2645c9ada9fcb8b3c31d84e643265f448bfff2
LakiEnt <91592081+LakiEnt@users.noreply.github.com>
2023-04-21 12:53:40 +0500

    настроил шрифты

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="body">
+    <div class="container"> 
+
+        <div class="main-page"> 
+            <div class="header"> 
+              <div class="left-side"> 
+                <div class="header-item">Главная</div>
+                <div class="header-item">Игры</div>
+                <div class="header-item">Новости</div>
+              </div>
+              <div class="right-side"> 
+                <div class="header-item">Войти</div>
+              </div>
+            </div>
+
+            <div class="list-games"> 
+
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+// import "nes.css/css/nes.min.css";
+
 export default {
   name: 'IndexPage'
 }
 </script>
 <style lang="scss" scoped>
+
 .body{
   margin: 0;
   height: 1145px;
     
-  background-image: url("../static/main-wallpaper.png");
+  background: url("../static/main-wallpaper.png") no-repeat fixed;
+  position: sticky;
 }
+.container{
+  margin: 0 auto;
+  
+  max-width: 900px;
+}
+.main-page{
+  position: relative;
+  top:200px;
+
+  width: 100%;
+  height: 934px;
+ 
+  background: rgba(167, 82, 3, 0.9);
+}
+.header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 80px;
+
+  background: #1B1B1B;
+}
+.left-side{
+  display: flex;
+  justify-content: space-around;
+}
+.right-side{
+  display: flex;
+  justify-content: space-between;
+}
+.header-item{
+  font-size: smaller;
+  color: white;
+  margin: 0 20px 0 20px;
+}
+
 </style>
