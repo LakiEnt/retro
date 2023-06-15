a0db353e59176a3d301831921b0c0d12567eeeac
LakiEnt <nik.modenoff@yandex.ru>
2023-06-15 19:35:40 +0500

    Авторизация

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,59 @@
 <template>
   <div class="body">
-    <div class="container"> 
+    <div class="container">
 
-        <div class="main-page"> 
-            <div class="header"> 
-              <div class="left-side"> 
+        <div class="main-page">
+            <div class="header">
+              <div class="left-side">
                 <div class="nes-text header-item">Главная</div>
                 <div class="header-item">Игры</div>
                 <div class="header-item">Новости</div>
               </div>
-              <div class="right-side"> 
+              <div class="right-side">
                 <div class="header-item" @click="dialog = true">Войти</div>
 
-                <v-dialog v-model="dialog">
-                 
+                <v-dialog v-model="dialog" width="auto">
 
-                  <v-card>
-                    123123
+                  <v-card style="width: 600px; height: 300px;">
+                    <v-card-title>
+                      Авторизация
+                    </v-card-title>
+
+                    <v-card-text>
+                      <v-form>
+                        <v-text-field
+                          v-model="nickname"
+                          :counter="25"
+                          label="Никнейм"
+                        ></v-text-field>
+
+                        <v-text-field
+                          v-model="password"
+                          :counter="25"
+                          label="Пароль"
+                        >
+
+                        </v-text-field>
+                      </v-form>
+                    </v-card-text>
+
+                    <v-card-actions>
+                      <v-row>
+                        <v-col cols="3">
+                          <v-btn color="orange" block @click="dialog=false, userLogin()"> Авторизоваться</v-btn>
+                        </v-col>
+
+                      </v-row>
+
+                    </v-card-actions>
+
                   </v-card>
                 </v-dialog>
               </div>
             </div>
 
-            <div class="list-games"> 
-              <div v-for="game in games" :key="game.id" class="game-container"> 
+            <div class="list-games">
+              <div v-for="game in games" :key="game.id" class="game-container">
                  {{ game.gameName }}
               </div>
             </div>
@@ -46,6 +76,8 @@ export default {
     return{
       games:[],
       dialog: false,
+      nickname:'',
+      password:'',
     }
   },
   methods:{
@@ -57,12 +89,26 @@ export default {
           const response = await this.$axios.post('/api/games/', request);
           // const response = await this.$api.showGames(request)
           this.games= response.data.message.data
-          console.log(response)          
+          console.log(response)
         }
         catch(err) {
           console.error(err)
         }
+      },
+
+    async userLogin(){
+      const request = {
+        nickname:this.nickname,
+        password:this.password,
       }
+      try {
+        const response = await this.$axios.post('/api/auth/login', request);
+        console.log(response)
+      }
+      catch(err) {
+        console.error(err)
+      }
+    }
   },
   async created(){
      await this.getGames()
@@ -75,13 +121,13 @@ export default {
 .body{
   margin: 0;
   height: 1145px;
-    
+
   background: url("../static/main-wallpaper.png") no-repeat fixed;
   position: sticky;
 }
 .container{
   margin: 0 auto;
-  
+
   max-width: 900px;
 }
 .main-page{
@@ -90,7 +136,7 @@ export default {
 
   width: 100%;
   height: 934px;
- 
+
   background: rgba(167, 82, 3, 0.9);
 }
 .header{
