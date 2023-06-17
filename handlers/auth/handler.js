6571a0d20ea7c530d0ca3047d22653ab6bc340d4
Antzu7 <antzu2002@gmail.com>
2023-06-17 19:03:18 +0500

    Добавление игр

--- handlers/auth/handler.js
+++ handlers/auth/handler.js
@@ -11,7 +11,7 @@ async function auth(object) {
 
     const password = object.password;
     const nickName = object.nickName;
-    const params = [ nickName ]
+    const params = [ nickName ];
 
     const query = `SELECT u."userPassword"
                FROM users u
@@ -53,55 +53,61 @@ async function register (object) {
         });
     });
 
+    let isBegin = false;
+
     const client = await pool.connect();
     const token = crypto.randomBytes(32).toString("hex");
 
     try {
-        client.query('BEGIN');
+      client.query('BEGIN');
+      isBegin = true;
 
-        const getUser = await client.query(`SELECT *
-                                            FROM users u
-                                            WHERE u."userNickName" = $1 OR u."userEmail" = $2`, [ object.userNickName, object.userEmail ]);
-        if (getUser.rowCount > 0) {
-            console.log('Ошибка при регестрации. Никнейм или почта заняты');
-            data.message = 'Никнейм или аочта заняты';
-            return data;
-        }
+      const getUser = await client.query(`SELECT *
+                                          FROM users u
+                                          WHERE u."userNickName" = $1 OR u."userEmail" = $2`, [ object.userNickName, object.userEmail ]);
+      if (getUser.rowCount > 0) {
+        console.log('Ошибка при регестрации. Никнейм или почта заняты');
+        data.message = 'Никнейм или аочта заняты';
+        return data;
+      }
 
-        const addUser = await client.query(`INSERT INTO users ("userNickName", "userEmail", "userPassword", "userCountry", "uuid")
-                                            VALUES ($1, $2, $3, $4, $5)
-                                            RETURNING "userId"`, [ object.userNickName, object.userEmail, hashedPassword, object.userCountry, token ]);
-        if (addUser.rowCount > 0) {
-            const userId = addUser.rows[0].userId;
-            const message = `${ process.env.EMAIL_BASEURL }/auth/verify/${ userId }/${ token }`;
-            console.log(message)
-            const isEmailSuccess = await mailer.sendEmail(object.userEmail, 'verify email', message);
+      const addUser = await client.query(`INSERT INTO users ("userNickName", "userEmail", "userPassword", "userCountry", "uuid")
+                                          VALUES ($1, $2, $3, $4, $5)
+                                          RETURNING "userId"`, [ object.userNickName, object.userEmail, hashedPassword, object.userCountry, token ]);
+      if (addUser.rowCount > 0) {
+        const userId = addUser.rows[0].userId;
+        const message = `${ process.env.EMAIL_BASEURL }/auth/verify/${ userId }/${ token }`;
+        console.log(message)
+        const isEmailSuccess = await mailer.sendEmail(object.userEmail, 'verify email', message);
 
-            if (isEmailSuccess) {
-                client.query('COMMIT');
-                data.message = 'success';
-                data.statusCode = 200;
-            }
-            else {
-                console.log('Не удалось отправить email');
-            }
+        if (isEmailSuccess) {
+          client.query('COMMIT');
+          isBegin = false
+          data.message = 'success';
+          data.statusCode = 200;
         }
         else {
-            client.query('ROLLBACK');
-            console.log('Не удалось добавить нового пользователя');
-            data.message = 'Не удалось добавить нового пользователя';
+            console.log('Не удалось отправить email');
         }
-
-    } catch (err) {
+      }
+      else {
         client.query('ROLLBACK');
-        console.error(err.message, err.stack);
+        isBegin = false;
+        console.log('Не удалось добавить нового пользователя');
+        data.message = 'Не удалось добавить нового пользователя';
+      }
+    } catch (err) {
+      if (isBegin) {
+        client.query('ROLLBACK');
+      }
+      console.error(err.message, err.stack);
     } finally {
-        client.release();
-        console.log('Release client');
+      client.release();
+      console.log('Release client');
     }
 
-    return data;
-};
+  return data;
+}
 
 async function verify (object) {
     const data = {
--- handlers/games/handler.js
+++ handlers/games/handler.js
@@ -1,35 +1,75 @@
 const pool = require('../../db.js')
 
 async function showGames(object) {
-    const data = {
-        message:    'ERROR',
-        statusCode: 400,
-    };
-    const limit = 10;
-    let page = object.page || 1;
-    let offset = page * limit - limit;
-    let params = [ limit, offset ];
-    const query = `SELECT * 
-             FROM games g
-             ORDER BY g."gameName"
-             LIMIT $1 OFFSET $2`;
-    
-    const client = await pool.connect();
-    try {
-        const result = await client.query(query, params);
-        data.message = { data: result.rows };
-        data.statusCode = 200;
-    } catch(err) {
-        console.error(err.message, err.stack);
-    } finally {
-        client.release();
-        console.log('Release client');
-    }
-    return data;
+  const data = {
+      message:    'ERROR',
+      statusCode: 400,
+  };
+  const limit = 10;
+  let page = object.page || 1;
+  let offset = page * limit - limit;
+  let params = [ limit, offset ];
+  const query = `SELECT *
+           FROM games g
+           ORDER BY g."gameName"
+           LIMIT $1 OFFSET $2`;
+
+  const client = await pool.connect();
+  try {
+      const result = await client.query(query, params);
+      data.message = { data: result.rows };
+      data.statusCode = 200;
+  } catch(err) {
+      console.error(err.message, err.stack);
+  } finally {
+      client.release();
+      console.log('Release client');
+  }
+  return data;
 }
 
+async function addGame(object) {
+  const data = {
+    message:    'ERROR',
+    statusCode: 400,
+  };
 
+  let isBegin = false
+  const client = await pool.connect();
+
+  try {
+    client.query('BEGIN');
+    isBegin = true;
+
+    const addGame = await client.query(`INSERT INTO games ("gameName", "gameDate", "gameDescription", "imageURL")
+                                        VALUES ($1, $2, $3, $4)`, [ object.name, object.date, object.description, object.imageURL ]);
+    if (addGame.rowCount > 0) {
+      client.query('COMMIT');
+      isBegin = false
+      data.message = 'success';
+      data.statusCode = 200;
+    }
+    else {
+      client.query('ROLLBACK');
+      isBegin = false;
+      console.log('Не удалось добавить новую игру');
+      data.message = 'Не удалось добавить новую игру';
+    }
+
+  } catch (err) {
+    if(isBegin) {
+      client.query('ROLLBACK');
+    }
+    console.error(err.message, err.stack);
+  } finally {
+    client.release();
+    console.log('Release client');
+  }
+
+  return data;
+}
 
 module.exports = {
     showGames: showGames,
+    addGame:   addGame,
 }
