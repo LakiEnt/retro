59d0e890b76e576b4e379a40e13e3117be27e1f1
Antzu7 <antzu2002@gmail.com>
2023-06-18 16:07:25 +0500

    то-то

--- handlers/speedrun/handler.js
+++ handlers/speedrun/handler.js
@@ -13,7 +13,8 @@ async  function showSpeedruns(object) {
 
   const client = await pool.connect();
   try {
-    const query = `SELECT u."userNickName",
+    const query = `SELECT s."speedrunId",
+                          u."userNickName",
                           to_char(TO_TIMESTAMP(s."speedrunTime"::double precision / 1000), 'HH24:MI:SS.MS') AS "speedrunTime",
                           to_char(s."speedrunDate", 'dd.mm.yyyy')                                           AS "speedrunDate",
                           u."userCountry"
@@ -36,6 +37,49 @@ async  function showSpeedruns(object) {
   return data;
 }
 
+async function addSpeedrun(object) {
+  const data = {
+    message:    'ERROR',
+    statusCode: 400,
+  };
+
+  let isBegin = false
+  const client = await pool.connect();
+
+  try {
+    client.query('BEGIN');
+    isBegin = true;
+
+    const addSpeedrun = await client.query(`INSERT INTO speedruns ("speedrunUser", "speedrunGame", "speedrunTime", "speedrunUrl")
+                                        VALUES ($1, $2, $3, $4)`, [ object.speedrunUser, object.speedrunGame, object.speedrunTime, object.speedrunUrl ]);
+
+    if (addSpeedrun.rowCount > 0) {
+      client.query('COMMIT');
+      isBegin = false
+      data.message = 'success';
+      data.statusCode = 200;
+    }
+    else {
+      client.query('ROLLBACK');
+      isBegin = false;
+      console.log('Не удалось добавить спидран в базу');
+      data.message = 'Не удалось добавить спидран в базу';
+    }
+
+  } catch (err) {
+    if(isBegin) {
+      client.query('ROLLBACK');
+    }
+    console.error(err.message, err.stack);
+  } finally {
+    client.release();
+    console.log('Release client');
+  }
+
+  return data;
+}
+
 module.exports = {
-  showSpeedruns: showSpeedruns
+  showSpeedruns: showSpeedruns,
+  addSpeedrun:   addSpeedrun,
 }
--- routes/speedrun/point.js
+++ routes/speedrun/point.js
@@ -33,6 +33,40 @@ module.exports = function (fastify, opts, next) {
     }
   })
 
+  fastify.route({
+    method: 'POST',
+    url: '/addSpeedrun',
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          speedrunUser: { type: 'integer' },
+          speedrunGame: { type: 'integer' },
+          speedrunTime: { type: 'integer' },
+          speedrunUrl:  { type: 'string' },
+        },
+        required: [ 'speedrunUser', 'speedrunGame', 'speedrunTime', 'speedrunUrl' ],
+      },
+      response: {
+        400: {
+          type:       'object',
+          properties: {
+            message:    { type: 'string' },
+            statusCode: { type: 'integer' },
+          },
+        },
+      }
+    },
+    async handler(request, reply) {
+      let data = await job.addSpeedrun(request.body)
+      if (data.statusCode !== 200) {
+        reply.status(400)
+      }
+
+      reply.send(data)
+    }
+  })
+
   next();
 }
 
